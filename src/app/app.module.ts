ef9a625cd08f4af72d268e383b568835d42049f5
luckyleonard <dzhu31@hotmail.com>
2020-08-23 21:46:12 +1000

    ✨ (feat) register and use HttpClient to send request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -11,8 +13,8 @@ import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
@@ -26,6 +28,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatFormFieldModule,
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
@@ -8,8 +9,17 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(public httpClient: HttpClient) {} //初始化httpClient 用来发送请求
+
   getPosts() {
-    return [...this.posts]; //解构返回copy
+    this.httpClient
+      .get<{ message: string; posts: Post[] }>(
+        'http://localhost:3000/api/posts'
+      )
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      }); //subscribe才会发起http请求
   }
 
   getPostsUpdatedListener() {
@@ -17,8 +27,13 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = { title, content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]); //更新observable
+    const post: Post = { id: null, title, content };
+    this.httpClient
+      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData.message);
+        this.posts.push(post); //更新本地数据
+        this.postsUpdated.next([...this.posts]); //更新observable
+      });
   }
 }
