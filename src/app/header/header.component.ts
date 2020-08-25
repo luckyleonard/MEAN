3ac74d65a12bd512995f8f36305563797a3b99bf
luckyleonard <dzhu31@hotmail.com>
2020-08-25 18:26:34 +1000

    💄 (scss) dress the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.componet.html',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   constructor() {}
