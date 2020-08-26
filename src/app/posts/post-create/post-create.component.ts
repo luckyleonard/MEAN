79ffed2529c4a71c605871d32e2a53a10f35cf71
luckyleonard <dzhu31@hotmail.com>
2020-08-26 14:13:32 +1000

    ✨ (feat) achieve edit logic with create post component - get param from url

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,22 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { PostsService } from './../post.service';
+import { Post } from './../post.model';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.scss'],
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
   // postTitle = '';
   // postContent = '';
-  constructor(public postsService: PostsService) {} //构建一个PostsService实例
-  onAddPost(postForm: NgForm) {
+  post: Post;
+  mode = 'create';
+  private postId: string;
+
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {} //构建一个PostsService实例 和 route实例获取params
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.postsService.getPost(this.postId).subscribe((postData) => {
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content,
+          };
+        });
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(postForm: NgForm) {
     if (postForm.invalid) {
       return;
     }
-    this.postsService.addPost(postForm.value.title, postForm.value.content); //调用postService的mutation
+    if (this.mode === 'create') {
+      this.postsService.addPost(postForm.value.title, postForm.value.content); //调用postService的mutation
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        postForm.value.title,
+        postForm.value.content
+      );
+    }
+
     postForm.resetForm();
   }
 }
